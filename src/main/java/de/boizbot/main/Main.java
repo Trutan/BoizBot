e44830a2f9b852ca6d256814cd4506d855282dbf
Maurice <bluedeetv@icloud.com>
2022-04-29 19:00:17 +0200

    Refactor Main

---
+++ src/main/java/de/boizbot/main/Main.java
@@ -0,0 +1,20 @@
+package de.boizbot.main;
+
+import de.boizbot.utils.Key;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    public static JDA shardMan;
+    public static JDABuilder builder;
+
+    public static void main(String[] args) throws LoginException {
+        builder = JDABuilder.createDefault(Key.token);
+        builder.setActivity(Activity.listening("Musik"));
+        builder.build();
+    }
+}
